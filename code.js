344a95f8b1c103ae7fd403edd8fdc645017b119c
Sahal Osman <engcaatoos2000@gmail.com>
2024-12-11 11:04:56 -0800

    array

---
+++ code.js
@@ -0,0 +1,9 @@
+<script>
+creating an array to practice
+const classes = ['CA2313', 'CA2314', 'CA2315', 'CA2316', 'CA2317', 'CA2318', 'CA2319'];
+// display array elements using traditional for loop
+for (let i=0; i< classes.length; i++) {
+    console.log(classes[i]);
+
+
+</script>
